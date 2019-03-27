815ae0bfb1aa0cce4e2c24f86c5a7e9695ea4d74
Chris Berns <cberns223@gmail.com>
2019-03-27 12:18:41 -0400

    Style Tweaks

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Player = () => (
   <div id="media">
-    <iframe src="https://open.spotify.com/embed/track/4gEAYcaZPHpFFelzdt7pBX" width="100%" height="100" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    <iframe src="https://open.spotify.com/embed/track/4gEAYcaZPHpFFelzdt7pBX" width="100%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
   </div>
 )
 
--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { Container, Segment } from 'semantic-ui-react';
 
 
 export const Playlist = ({}) => (
-  <iframe src="https://open.spotify.com/embed/user/nsqx6avzxhybkmfumqb1juwmn/playlist/5OJs7eATLrvZ2Ea9als3lK" width="100%" height="500" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+  <Segment inverted >
+    <iframe src="https://open.spotify.com/embed/user/nsqx6avzxhybkmfumqb1juwmn/playlist/5OJs7eATLrvZ2Ea9als3lK" width="100%" height="430" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>  
+  </Segment>
 )
