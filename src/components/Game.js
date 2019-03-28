a37f2408d5929313d7b782b5b308b6b30f6c7832
Chris Berns <cberns223@gmail.com>
2019-03-28 10:16:29 -0400

    integrated tapBeat

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react'
 import { Header, Segment } from 'semantic-ui-react'
 import Jumper from './Jumper'
+import {gameStart} from '../games/TabBeat'
+// import '../css/TabBeat'
+
 
 const GAME_STATUS = {
   notStated: "NOT_STARTED",
 }
 
-export default class Game extends Component{
-  constructor(props){
+export default class Game extends Component {
+  constructor(props) {
     super(props);
     // You can initialize state properties here
     this.state = {
       gameStatus: GAME_STATUS.notStated
     }
   }
-  render(){
+
+  componentDidMount() {
+    gameStart()
+  }
+  render() {
     return (
       <Segment id="game" inverted>
         <Header as='h1' textAlign='center'>
-        Game
+          Game
         </Header>
-       <Jumper />
+        <div id="game">
+          <h1 id="header">Tap Beat Game</h1>
+          <p>Objective: Tap the buttons in the given time period.</p>
+
+          <button onclick="gameStart()">Start</button>
+        </div>
+        {/* <Jumper /> */}
       </Segment>
     )
   }
