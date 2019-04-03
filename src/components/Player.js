b52b6a98c802d37c7f4f05d99d58b2bd5cf70d4c
Raphael Megali <ramegali23@gmail.com>
2019-04-03 08:32:13 -0400

    Added player controls. CSS is a mess, but music playback and Spotify authentication are working as intended.

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,13 +1,17 @@
 // tutorial used: https://levelup.gitconnected.com/how-to-build-a-spotify-player-with-react-in-15-minutes-7e01991bc4b6
 
 import React, { Component } from "react";
-import * as $ from "jquery";
 import { authEndpoint, clientId, redirectUri, scopes } from "../constants/Config";
 import hash from "../actions/Hash";
-import Player from "./PlayerController";
 // import logo from "./logo.svg";
 // import "./App.css";
 import "../css/player.css";
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 
 export default class App extends Component {
   constructor() {
@@ -30,7 +34,6 @@ export default class App extends Component {
       position: 0,
       duration: 1,
     };
-    // this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
   }
 
   componentDidMount() {
@@ -49,12 +52,12 @@ export default class App extends Component {
   }
 
   async createPlayer(_token){
-    console.log("about to await");
-    await window.Spotify;
-    console.log("we awaited");
+    // await window.Spotify;
 
     // if the Spotify SDK has loaded
-    if (!!window.Spotify) { 
+    while (!window.Spotify) { 
+      await sleep(30)
+    }
       console.log("creating new Spotify player with token " + _token);
       // create a new player
       this.player = new window.Spotify.Player({
@@ -66,12 +69,6 @@ export default class App extends Component {
       
       // finally, connect!
       this.player.connect();
-    }
-
-    this.getCurrentlyPlaying(_token)
-    .then(() => {
-      this.transferPlaybackHere();
-    });
   }
 
   createEventHandlers = () => {
@@ -92,39 +89,16 @@ export default class App extends Component {
     this.player.on('player_state_changed', state => this.onStateChanged(state));
   
     // Ready
-    // this.player.on('ready', async data => {
-    //   let { device_id } = data;
-    //   // set the deviceId variable, then let's try
-    //   // to swap music playback to *our* player!
-    //   await this.setState({ deviceId: device_id });
-    //   this.transferPlaybackHere();
-    // });
-  }
-
-  getCurrentlyPlaying = (token) => {
-    // Make a call using the token
-    $.ajax({
-      url: "https://api.spotify.com/v1/me/player",
-      type: "GET",
-      beforeSend: (xhr) => {
-        xhr.setRequestHeader("Authorization", "Bearer " + token);
-      },
-      success: (data) => {
-        console.log("data", data);
-        if (!!data) {
-          this.setState({
-            item: data.item,
-            is_playing: data.is_playing,
-            progress_ms: data.progress_ms,
-            deviceId: data.device.id,
-          });
-        }
-      }
+    this.player.on('ready', async data => {
+      let { device_id } = data;
+      // set the deviceId variable, then try
+      // to swap music playback to Juke
+      await this.setState({ deviceId: device_id });
+      this.transferPlaybackHere(device_id);
     });
   }
 
-  transferPlaybackHere = () => {
-    // const { deviceId, token } = this.state.token;
+  transferPlaybackHere = (deviceId) => {
     // https://beta.developer.spotify.com/documentation/web-api/reference/player/transfer-a-users-playback/
     fetch("https://api.spotify.com/v1/me/player", {
       method: "PUT",
@@ -133,7 +107,7 @@ export default class App extends Component {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        "device_ids": [ this.state.deviceId ], //deviceId ],
+        "device_ids": [ deviceId ],
         // true: start playing music if it was paused on the other device
         // false: paused if paused on other device, start playing music otherwise
         "play": true,
@@ -141,12 +115,48 @@ export default class App extends Component {
     });
   }
 
+   // when we receive a new update from the player
+   onStateChanged = (state) => {
+    // only update if we got a real state
+    if (state !== null) {
+      const {
+        current_track: currentTrack,
+        position,
+        duration,
+      } = state.track_window;
+      const is_playing = !state.paused;
+      
+      this.setState({
+        position,
+        duration,
+        item: currentTrack,
+        is_playing
+      });
+    } else {
+      // state was null, user might have swapped to another device
+      this.setState({ error: "Looks like you might have swapped to another device?" });
+    }
+  }
+
+  onPrevClick = () => {
+    this.player.previousTrack();
+  }
+  
+  onPlayClick = () => {
+    this.player.togglePlay();
+  }
+  
+  onNextClick = () => {
+    this.player.nextTrack();
+  }
+
   render() {
+    const { is_playing } = this.state;
 
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          {/* Get token */}
           {!this.state.token && (
             <a
               className="btn btn--loginApp-link"
@@ -157,36 +167,29 @@ export default class App extends Component {
               Login to Spotify
             </a>
           )}
+          {/* Display player once token acquired */}
           {this.state.token && (
-            // <Player
-            //   token={this.state.token}
-            //   item={this.state.item}
-            //   is_playing={this.state.is_playing}
-            //   progress_ms={this.progress_ms}
-            // />
             <div className="App">
-      <div className="main-wrapper">
-        <div className="now-playing__img">
-          <img src={this.state.item.album.images[0].url} />
-        </div>
-        <div className="now-playing__side">
-          <div className="now-playing__name">{this.state.item.name}</div>
-          <div className="now-playing__artist">
-            {this.state.item.artists[0].name}
-          </div>
-          <div className="now-playing__status">
-            {this.state.is_playing ? "Playing" : "Paused"}
-          </div>
-          {/* <div className="progress">
-            <div
-              className="progress__bar"
-              style={progressBarStyles}
-            />
-          </div> */}
-        </div>
-        {/* <div className="background" style={backgroundStyles} />{" "} */}
-      </div>
-    </div>
+              <div className="main-wrapper">
+                <div className="now-playing__img">
+                  <img src={this.state.item.album.images[0].url} />
+                </div>
+                <div className="now-playing__side">
+                  <div className="now-playing__name">{this.state.item.name}</div>
+                  <div className="now-playing__artist">
+                    {this.state.item.artists[0].name}
+                  </div>
+                  <div className="now-playing__status">
+                    {this.state.is_playing ? "Playing" : "Paused"}
+                  </div>
+                  <p>
+                    <button onClick={() => this.onPrevClick()}>Previous</button>
+                    <button onClick={() => this.onPlayClick()}>{is_playing ? "Pause" : "Play"}</button>
+                    <button onClick={() => this.onNextClick()}>Next</button>
+                  </p>
+                </div>
+              </div>
+            </div>
           )}
         </header>
       </div>
