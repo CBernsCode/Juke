5cf8d8ecb20ca04242272c4b910667b117ade103
Gordon Wu <gordon_wu@student.uml.edu>
2019-04-30 11:40:39 -0400

    Added Pretty Colors

--- src/games/TabBeat.js
+++ src/games/TabBeat.js
@@ -10,6 +10,17 @@ export const TapBeat = () => {
 
   let game = document.getElementById("game");
 
+  let colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+                '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+                '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+                '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+                '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+                '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+                '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+                '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+                '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+                '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+
   window.kill = () => {
     let oldScore = score
     score = 0
@@ -38,6 +49,7 @@ export const TapBeat = () => {
       buttonTest.style.top = Math.floor((Math.random() * 200) + 100) + "px";
       buttonTest.style.left = Math.floor((Math.random() * 200) + 300) + "px";
       buttonTest.style.position = "absolute";
+      buttonTest.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
   
       if (buttonNum === 9) {
         buttonNum = 0;
